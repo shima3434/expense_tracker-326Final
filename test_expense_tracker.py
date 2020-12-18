3c094bf4716a53c86f1585cbad8e45cebb7ade83
Shima Abdulla <shima1@terpmail.umd.edu>
2020-12-17 21:01:23 -0500

    added in file with methods and some docstrings for test file

---
+++ test_expense_tracker.py
@@ -0,0 +1,21 @@
+#Names: Shima Abdulla, Do Yun Kim, Burhan Marvi, Joseph Sanchez
+""" Test expense tracker script """
+
+import expense_tracker as exp_track
+
+from expense_tracker import Expenses, record_expenses, ideal_expenses
+
+def test_ideal_expenses():
+     
+def test_percentage():
+    """ Some happy path cases to test the percentage function"""
+
+def test_most_expense():
+    """ Some happy path cases to test the most expense function"""
+    
+def test_compare_happy_path():
+    """ Some happy path cases to test the compare function """
+
+def test_compare_happy_edge():
+    """ Some edge cases to test the compare function """
+
